bb2c668f3c9afab740106cfcb3bddc84d12395e9
Trong Nguyen <33231414+nguyentrong56@users.noreply.github.com>
2021-11-29 14:13:54 -0800

    Initial commit

---
+++ src/Prime.java
@@ -0,0 +1,11 @@
+public final class Prime {
+	public static boolean isPrime(int number) {
+		for(int i = 2; i <= number/2; i++) {
+			if (number % i == 0) {
+				return false;
+			}
+		}
+		
+		return true;
+	}
+}
